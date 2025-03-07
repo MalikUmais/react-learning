69b046989b6b417f7895529132f7b5a2cae42a0b
MalikUmais <umaisfaruq03@gmail.com>
2025-03-07 23:39:03 +0500

    commit

--- Project Todo version 3/src/App.jsx
+++ Project Todo version 3/src/App.jsx
@@ -3,40 +3,18 @@ import AppName from "./components/AppName";
 import TodoItems from "./components/TodoItems";
 import AddTodo from "./components/AddTodo";
 import "./App.css";
-import { useState } from "react";
+import TodoItemsContextProvider from "./store/todo-items-store";
 import WelcomeMessage from "./components/WelcomeMessaage";
-import { TodoItemsContext } from "./store/todo-items-store";
 function App() {
-  const [todoItems, setTodoItems] = useState([]);
-  const addNewItem = (itemName, itemDueDate) => {
-    setTodoItems((currValue) => {
-      const newTodoItems = [
-        ...currValue,
-        { name: itemName, dueDate: itemDueDate },
-      ];
-      return newTodoItems;
-    });
-  };
-  const deleteItem = (todoItemName) => {
-    const newTodoItems = todoItems.filter((item) => item.name !== todoItemName);
-    setTodoItems(newTodoItems);
-  };
-
   return (
-    <TodoItemsContext.Provider
-      value={{
-        todoItems,
-        addNewItem,
-        deleteItem,
-      }}
-    >
+    <TodoItemsContextProvider>
       <center className="todocontainer">
         <AppName />
         <AddTodo></AddTodo>
         <WelcomeMessage></WelcomeMessage>
         <TodoItems></TodoItems>
       </center>
-    </TodoItemsContext.Provider>
+    </TodoItemsContextProvider>
   );
 }
 
