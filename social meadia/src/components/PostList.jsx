1c1c00a2a90dee7a103899a97d83c6bf19cfbc31
MalikUmais <umaisfaruq03@gmail.com>
2025-03-09 22:32:57 +0500

    comit

--- social meadia/src/components/PostList.jsx
+++ social meadia/src/components/PostList.jsx
@@ -1,26 +1,28 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import Post from "./Post";
 import { PostList as PostListData } from "../store/post-list-store";
 import WelcomeMessage from "./WelcomeMessage";
 
 const PostList = () => {
   const { postList, addInitialPosts } = useContext(PostListData);
-  const handleGetPostsClick = () => {
-    fetch("https://dummyjson.com/posts")
-      .then((res) => res.json())
-      .then((data) => {
-        const modifiedPosts = data.posts.map(post => ({
-          ...post,
-          reactions: post.reactions.likes // Extract only likes
-        }));
-        addInitialPosts(modifiedPosts);
-      });
-  };
+useEffect(() => {
+  fetch("https://dummyjson.com/posts")
+    .then((res) => res.json())
+    .then((data) => {
+      const modifiedPosts = data.posts.map((post) => ({
+        ...post,
+        reactions: post.reactions.likes, // Extract only likes
+      }));
+      addInitialPosts(modifiedPosts);
+    });
+  
+}, []);
+  
+
+  const handleGetPostsClick = () => {};
   return (
     <>
-      {postList.length === 0 && (
-        <WelcomeMessage onGetPostsClick={handleGetPostsClick}></WelcomeMessage>
-      )}
+      {postList.length === 0 && <WelcomeMessage></WelcomeMessage>}
       {postList.map((post) => (
         <Post key={post.id} post={post} />
       ))}
--- social meadia/src/components/WelcomeMessage.jsx
+++ social meadia/src/components/WelcomeMessage.jsx
@@ -2,9 +2,7 @@ const WelcomeMessage = ({onGetPostsClick}) => {
   return (
     <center className="welcome-message">
       <h1 className="text-center ">No Posts</h1>
-      <button type="button" className="btn btn-secondary" onClick={onGetPostsClick}>
-        Fetch Posts
-      </button>
+      
     </center>
   );
 };
