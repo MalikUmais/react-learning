789850ee0a75183f8f35b632caf39a1ef88162eb
MalikUmais <umaisfaruq03@gmail.com>
2025-03-10 22:32:10 +0500

    commit

--- social meadia/src/components/PostList.jsx
+++ social meadia/src/components/PostList.jsx
@@ -1,29 +1,36 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import Post from "./Post";
 import { PostList as PostListData } from "../store/post-list-store";
 import WelcomeMessage from "./WelcomeMessage";
+import LoadingSpinner from "./LoadingSpinner";
 
 const PostList = () => {
   const { postList, addInitialPosts } = useContext(PostListData);
-useEffect(() => {
-  fetch("https://dummyjson.com/posts")
-    .then((res) => res.json())
-    .then((data) => {
-      const modifiedPosts = data.posts.map((post) => ({
-        ...post,
-        reactions: post.reactions.likes, // Extract only likes
-      }));
-      addInitialPosts(modifiedPosts);
-    });
-  
-}, []);
-  
+  const [fetching, setFetching] = useState(false);
+  useEffect(() => {
+    setFetching(true);
+    const controller=new AbortController();
+    const signal=controller.signal;
+    fetch("https://dummyjson.com/posts",{signal})
+      .then((res) => res.json())
+      .then((data) => {
+        const modifiedPosts = data.posts.map((post) => ({
+          ...post,
+          reactions: post.reactions.likes, // Extract only likes
+        }));
+        addInitialPosts(modifiedPosts);
+        setFetching(false);
+      });
+      return () => {
+        controller.abort();
+      }
+  }, []);
 
-  const handleGetPostsClick = () => {};
   return (
     <>
-      {postList.length === 0 && <WelcomeMessage></WelcomeMessage>}
-      {postList.map((post) => (
+      {fetching && <LoadingSpinner/>}
+      {!fetching && postList.length === 0 && <WelcomeMessage></WelcomeMessage>}
+      {!fetching && postList.map((post) => (
         <Post key={post.id} post={post} />
       ))}
     </>
