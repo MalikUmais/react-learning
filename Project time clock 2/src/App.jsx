944b84191585f78d440a8f031d3977a1f0df3a88
MalikUmais <umaisfaruq03@gmail.com>
2025-03-10 22:47:48 +0500

    commit

---
+++ Project time clock 2/src/App.jsx
@@ -0,0 +1,15 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import Heading from './components/Heading'
+import Slogan from './components/Slogan'
+import Time from './components/Time'
+function App() {
+  return (
+    <center>
+      <Heading/>
+      <Slogan/>
+      <Time/>
+    </center>
+  )
+}
+
+export default App
---
+++ Project time clock 2/src/components/Slogan.jsx
@@ -0,0 +1,4 @@
+let Slogan=()=>{
+    return <p className="lead">This clock shows the time like all other clocks😂</p>
+}
+export default Slogan;
